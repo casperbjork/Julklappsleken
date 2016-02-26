7b9e2436eebbfed3483935b6cffe6d0fc8fe5660
Casper Björk <cabbe29@hotmail.com>
2016-02-26 22:05:22 +0100

    Added cookies!
    One huge step forward to a finished state :)

--- public_html/pregamejs.js
+++ public_html/pregamejs.js
@@ -1,4 +1,7 @@
-// todo  Generator för den unika spelare med "bilder" o namn...  Cookie skapare... 
+//Cookie skapare... 
+
+var text = ["Sista steget innan vi kan börja spela. <br /> <br /> Välj en tid som ni vill spela runt. Datorn kommer att sätta en specifik tid som ingen får veta. <br /> <br /> När ni känner er klara så klickar ni på <b>Börja spela</b> knappen."];
+
 
 function submitnumplayers(){
     var input = document.getElementById("inputnumplayers").value;
@@ -9,6 +12,7 @@ function submitnumplayers(){
             if (input === "1") throw "Du måste ha minst två spelare.";
             if (input === "") throw "Rutan är tom.";
             if (isNaN(input)) throw "Du måste använda siffror för att skriva in antalet spelare.";
+            if (input>100) throw "Det blir inte bra att ha över 100 personer som spelar, om du verkligen vill kontakta mig...";
         }
         catch(e) {
             alertTextDiv.style.display = "block";
@@ -19,7 +23,7 @@ function submitnumplayers(){
     playerSetting(input);
     alertTextDiv.style.display = "none";
     document.getElementById("steg1").innerHTML = "Uppdatera";
-    document.getElementById("steg2div").style.display = "inline";     
+    document.getElementById("stage2div").style.display = "inline";     
 }
 
 
@@ -71,6 +75,85 @@ function playerSetting(numplayers){
             okName(this);
         });
     }
+    var cookies = document.cookie.split(";");
+    for (var x = 0; x < cookies.length; x++) {
+        var cookieexist = checkPlayerCookie(x+1);
+        console.log(x+1);
+        if (cookieexist === true) {
+            var name = getPlayerName(x+1);
+            console.log(name);
+            var text = document.createElement("p");
+            text.innerHTML = name; 
+
+            document.getElementById("player"+(x+1)).replaceChild(text, document.getElementById("player"+(x+1)).firstChild);
+
+        }
+    }     
+}
+
+function createPlayerCookie(playernum, playername) {
+    var currdate = new Date();
+    var expiredate = currdate.setHours(currdate.getHours()+1);
+    document.cookie="player"+playernum +"=" +playername +"; expires="+expiredate;
+}
+
+function checkPlayerCookie(playernum) {
+    var cookies = document.cookie.split(";");
+    if (playernum >= 10) {
+        for (var x = 10; x < cookies.length; x++){
+           if (cookies[x].substr(7, 2) == playernum) {
+               return true;
+           }
+        }
+    }
+    if (cookies[0].substr(6, 1) == playernum) {
+        return true;
+    }
+    for (var x = 1; x < cookies.length; x++){
+        if (cookies[x].substr(7, 1) == playernum) {
+           return true;
+       }
+   }
+       return false;
+}
+
+function checkPlayerName(playername) {
+    var cookies = document.cookie.split(";");
+        if (cookies[0].substr(8) === playername) {
+            return true;
+        }
+ 
+       for (var x = 1; x < cookies.length; x++){
+           if (cookies[x].substr(9) === playername) {
+               return true;
+           } else {
+               
+           }
+        }
+    return false;
+}
+
+function getPlayerName(playernum) {
+    var cookies = document.cookie.split(";");
+    var playername = " ";
+    if (playernum >= 10) {
+        for (var x = 10; x < cookies.length; x++){
+           if (cookies[x].substr(7, 2) == playernum) {
+               playername = cookies[x].substr(10);
+               return playername;
+           }
+        }
+    }
+        if (cookies[0].substr(6, 1) == playernum) {
+            playername = cookies[0].substr(8);
+            return playername;
+        }
+       for (var x = 1; x < cookies.length; x++){
+           if (cookies[x].substr(7, 1) == playernum) {
+               playername = cookies[x].substr(9);
+               return playername;
+           }
+        }
 }
 
 
@@ -83,13 +166,32 @@ function clearPlayerSetting() {
 }
 
 function okName(btn) {
+    var alertText = document.getElementById("inputAlertS1");
+    var alertTextDiv = document.getElementById("inputAlertS1Div");
     var clickbtn = btn.id;
     var player = clickbtn.substr(9);
     var inputtext = document.getElementById("playerinput"+player).value;
+    var namecheck = checkPlayerName(inputtext);
+        if (namecheck === true) {
+            alertTextDiv.style.display = "block";
+            alertText.innerHTML = "Det finns redan en spelare som heter "+ inputtext;
+            return "";
+        }
+    createPlayerCookie(player, inputtext);
+    alertTextDiv.style.display = "none";
     var text = document.createElement("p");
         text.innerHTML = inputtext; 
     
     document.getElementById("player"+player).replaceChild(text, document.getElementById("player"+player).firstChild);
-        
-    
 }
+
+function stage2() {
+    clearPlayerSetting();
+    document.getElementById("stage1pre").style.display = "none";
+    document.getElementById("stage2div").style.display = "none";
+    
+        document.getElementById("infoTextPreGame").innerHTML = text[0];
+}
+
+//Skapa function som lägger tillbaka spelarna vid återanslutning, Skapa också en knapp som tar bort cookien.
+//Cookien ska fylla i allt för dem.
